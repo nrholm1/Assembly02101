84d074bb2eb401f39cac7152bc2e642aa0e5a497
nrholm1 <nielsraunkjaer@gmail.com>
2020-11-06 11:41:07 +0100

    initial commit -> gates and Mult.asm

---
+++ HackAssembly/Mult.asm
@@ -0,0 +1,33 @@
+@R1 // counter input
+D=M
+@counter
+M=D // counter = R1
+@END
+D;JEQ
+
+@R0 // base input
+D=M
+@base 
+M=D // base = R0
+
+(LOOP)
+    @base
+    D=M
+
+    @R2 // output -> product of base and counter
+    M=M+D // increment output by base
+
+    @counter
+    D=M
+    M=D-1
+    D=M
+    @LOOP
+    D;JGT
+    @END
+    0;JMP
+(END)
+    @END
+    0;JMP
+
+
+    
---
+++ HackAssembly/test.asm
@@ -0,0 +1,10 @@
+@1
+M-1
+@7
+D=A
+D=D+M
+@9
+M-D
+(GOON)
+@GOON
+0;JMP
