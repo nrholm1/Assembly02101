5a89ffbdf4f8034d23236cee9b6c6d3273757c4b
Niels Raunkjær <NielsRHolm@nielss-mbp-2.eduroam.wireless.dtu.dk>
2020-11-20 11:34:12 +0100

    Implemented div.asm

--- HackAssembly/Div.asm
+++ HackAssembly/Div.asm
@@ -12,3 +12,44 @@
 
 // terminate by inf loop
 
+@R2
+@output
+D=0 // set output = 0
+M=D
+
+@R0 // input
+D=M
+@input
+M=D // input = R0
+@END
+D;JEQ
+
+@R1 // divisor input
+D=M
+@divisor 
+M=D // divisor = R0
+@END
+D;JEQ // division by 0!
+
+(LOOP)
+    @divisor
+    D=M
+
+    @input
+    M=M-D // subtract divisor from input
+
+    // @input
+    D=M
+    @END
+    D;JLT
+
+    @output
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+    
+(END)
+    @END
+    0;JMP
